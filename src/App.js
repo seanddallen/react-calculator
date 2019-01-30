b793aff9879c77c87a2755798dc36f4c071edb70
Sean Tayler <seanjtayler@gmail.com>
2019-01-30 14:46:30 -0700

    fixed errors

--- src/App.js
+++ src/App.js
@@ -12,10 +12,42 @@ class App extends Component {
   }
 
   handleUpdateInput = (val) => {
-    this.setState({input: this.state.input + val})
+    const operators = ["+", "-", "*", "/"]
+    const { input } = this.state
+
+    if(input === ""){
+      if(operators.includes(val)){
+        return;
+      }
+    }
+
+    if((input[input.length-1] === "+"
+      || input[input.length-1] === "-"
+      || input[input.length-1] === "/"
+      || input[input.length-1] === "*")
+      && operators.includes(val)){
+      return;
+    }
+
+    this.setState({input: input === '0' ? val : input + val})
+  }
+
+  handleDotInput = (val) => {
+    if(this.state.input.indexOf('.') === -1){
+      this.setState({input: this.state.input + val})
+    }
   }
 
   handleEqual = (val) => {
+    const { input } = this.state
+
+    if(input[input.length-1] === "+"
+      || input[input.length-1] === "-"
+      || input[input.length-1] === "/"
+      || input[input.length-1] === "*"
+      || input[input.length-1] === "."){
+        return; 
+      }
     this.setState({input: math.eval(this.state.input)})
   }
 
@@ -43,7 +75,7 @@ class App extends Component {
             <Button handleClick={this.handleUpdateInput}>+</Button>
           </div>
           <div className="row">
-            <Button handleClick={this.handleUpdateInput}>.</Button>
+            <Button handleClick={this.handleDotInput}>.</Button>
             <Button handleClick={this.handleUpdateInput}>0</Button>
             <Button handleClick={this.handleEqual}>=</Button>
             <Button handleClick={this.handleUpdateInput}>-</Button>
